aeb7208483855421f1fe2265b9d32f908f350903
tinhtran12345 <tinhtran04012002@gmail.com>
2023-01-07 17:23:06 +0700

    add app.js

--- Draggable_Image_Slider/app.js
+++ Draggable_Image_Slider/app.js
@@ -1,1 +1,50 @@
-// Draggable Image Slider
+const carousel = document.querySelector(".carousel")
+firstImg = carousel.querySelectorAll("img")[0];
+
+arrowIcons = document.querySelectorAll(".container i")
+
+let isDragStart =false, prevPageX, prevScrollLeft;
+let firstImgWidth  = firstImg.clientWidth + 14;
+let scrollWidth = carousel.scrollWidth -carousel.clientWidth;
+const showHideIcons = ()=>{
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none":"block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none":"block";
+}
+
+arrowIcons.forEach(icon =>{
+    icon.addEventListener("click", ()=>{
+        // console.log(icon);
+        carousel.scrollLeft += icon.id == "left"? -firstImgWidth:firstImgWidth;
+        setTimeout(()=>showHideIcons(), 60);
+    })
+})
+
+
+
+
+const dragStart = (e)=>{
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e)=>{
+    if(!isDragStart) return ;
+    e.preventDefault();
+    carousel.classList.add('dragging');
+    let positionDiff = e.pageX-prevPageX;
+    carousel.scrollLeft =prevScrollLeft-positionDiff;
+    showHideIcons()
+
+}
+
+const dragStop = ()=>{
+    isDragStart = false;
+    carousel.classList.remove("dragging")
+}
+
+
+
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+carousel.addEventListener("mouseup", dragStop)
